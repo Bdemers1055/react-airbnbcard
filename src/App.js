de9ec18858cc60d5c6b2fada1e6d5a6e98a71f14
bdemers1055 <bdemers1055@gmail.com>
2018-07-11 20:07:41 -0400

    stubbed out html for cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <section>
+         <div className="card">
+              <img src="https://via.placeholder.com/250x250" alt="card"/>
+              <div className="preheading">
+                <span className="category">River Rafting</span>
+                <span className="state"> Colorado</span>
+              </div>
+              <div className="heading">
+                <h3>title goes here</h3>
+              </div>  
+              <div className="postheading">
+                <small><span className="price">$00</span> per person</small>
+              </div>              
+              <div className="ratings">
+              <span className="rate stars">
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              </span>
+              <span className="reviewCount"> 10</span>
+              </div>
+         </div>
+       </section>
       </div>
     );
   }
